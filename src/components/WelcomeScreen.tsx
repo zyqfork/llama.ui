1ce1397e68e226e7706a9127c3327f42ae6452d0
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-09-04 13:58:49 +0300

    review: Code review

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -9,6 +9,8 @@ import { MessageExtra } from '../utils/types';
 import { ChatInput } from './ChatInput.tsx';
 import StorageUtils from '../utils/storage.ts';
 
+const SAMPLE_PROMPTS_COUNT = 4;
+
 export default function WelcomeScreen() {
   const navigate = useNavigate();
   const { sendMessage } = useMessageContext();
@@ -29,30 +31,26 @@ export default function WelcomeScreen() {
   };
 
   return (
-    <div className="flex flex-col h-full">
-      {/* main content area */}
-      <div className="grow flex flex-col overflow-y-auto px-2">
-        <div className="grid xl:gap-8 grow transition-[300ms]">
-          <div className="flex flex-col w-full xl:max-w-[900px] mx-auto">
-            <div className="grow flex flex-col items-center justify-center">
-              <b className="text-4xl">{lang.chatScreen.welcome}</b>
-              <small>{lang.chatScreen.welcomeNote}</small>
+    <div className="flex flex-col h-full w-full xl:max-w-[900px] mx-auto">
+      <div className="grow flex flex-col items-center justify-center px-2 transition-[300ms]">
+        <h1 className="text-4xl font-medium">{lang.chatScreen.welcome}</h1>
+        <small>{lang.chatScreen.welcomeNote}</small>
 
-              <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 max-w-5/6 sm:max-w-3/4 mt-8">
-                {getUniqueRandomElements(lang.samplePrompts, 4).map((text) => (
-                  <button
-                    key={text}
-                    className="btn h-auto bg-base-200 font-medium rounded-xl p-2"
-                    onClick={() => {
-                      navigate(`/chat?q=${encodeURIComponent(text)}`, {});
-                    }}
-                  >
-                    {text}
-                  </button>
-                ))}
-              </div>
-            </div>
-          </div>
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 max-w-5/6 sm:max-w-3/4 mt-8">
+          {getUniqueRandomElements(
+            lang.samplePrompts,
+            SAMPLE_PROMPTS_COUNT
+          ).map((text) => (
+            <button
+              key={text}
+              className="btn h-auto bg-base-200 font-medium rounded-xl p-2"
+              onClick={() => {
+                navigate(`/chat?q=${encodeURIComponent(text)}`, {});
+              }}
+            >
+              {text}
+            </button>
+          ))}
         </div>
       </div>
 
