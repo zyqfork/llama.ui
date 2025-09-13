35d8463200cbe4caef40f3ee48dcfe0705400347
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-09-13 14:01:28 +0300

    fix: PWA - update url pattern rules

--- vite.config.ts
+++ vite.config.ts
@@ -59,10 +59,13 @@ export default defineConfig({
       workbox: {
         runtimeCaching: [
           {
-            urlPattern: '\\.(?:html|css|js|mjs)$',
+            urlPattern: ({ request }) =>
+              request.destination === 'document' ||
+              request.destination === 'script' ||
+              request.destination === 'style',
             handler: 'StaleWhileRevalidate',
             options: {
-              cacheName: 'static-resources',
+              cacheName: 'static-cache',
               expiration: {
                 maxEntries: 50,
                 maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
@@ -70,18 +73,18 @@ export default defineConfig({
             },
           },
           {
-            urlPattern: '\\.(?:jpg|jpeg|png|svg|ico)$',
+            urlPattern: ({ request }) => request.destination === 'image',
             handler: 'CacheFirst',
             options: {
               cacheName: 'images-cache',
               expiration: {
-                maxEntries: 50,
+                maxEntries: 100,
                 maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
               },
             },
           },
           {
-            urlPattern: '\\.(?:woff2?|ttf|otf)$',
+            urlPattern: ({ request }) => request.destination === 'font',
             handler: 'CacheFirst',
             options: {
               cacheName: 'font-cache',
