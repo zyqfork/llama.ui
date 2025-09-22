6bdc4f88430550a10c4795c7cbdd6feab676e1d6
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-09-22 15:34:18 +0300

    review: use browser language detector

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { isDev } from '../config';
 
 import en from './en.json';
@@ -19,18 +20,21 @@ export const SUPPORTED_LANGUAGES = [
   { key: 'ar', name: 'Arabic', label: 'اَلْعَرَبِيَّةُ' },
 ];
 
-i18n.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'en',
-  debug: isDev,
-  interpolation: {
-    escapeValue: false,
-  },
-  resources: {
-    en: {
-      translation: en,
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: SUPPORTED_LANGUAGES.map((lang) => lang.key),
+    fallbackLng: 'en',
+    debug: isDev,
+    interpolation: {
+      escapeValue: false,
     },
-  },
-});
+    resources: {
+      en: {
+        translation: en,
+      },
+    },
+  });
 
 export default i18n;
