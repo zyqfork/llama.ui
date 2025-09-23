d70dcb5a7c0db633bfddd34c6cdad29c3e65234e
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-09-23 22:40:45 +0300

    feat: WelcomeScreen - translation

--- src/pages/WelcomeScreen.tsx
+++ src/pages/WelcomeScreen.tsx
@@ -52,10 +52,10 @@ export default function WelcomeScreen() {
     <div className="flex flex-col h-full w-full xl:max-w-[900px] mx-auto">
       <div className="grow flex flex-col items-center justify-center px-2 transition-[300ms]">
         <h1 className="text-4xl font-medium">
-          <Trans i18nKey="chatScreen.welcome" />
+          <Trans i18nKey="welcomeScreen.welcome" />
         </h1>
         <small>
-          <Trans i18nKey="chatScreen.welcomeNote" />
+          <Trans i18nKey="welcomeScreen.welcomeNote" />
         </small>
 
         <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 max-w-5/6 sm:max-w-3/4 mt-8">
